817041e2bbd83430f5d56cd16534e5214b3ce6bf
Rawesh Chedi <99039154@mydavinci.nl>
2019-04-21 23:28:52 +0200

    Controller and model settings

---
+++ PerformanceApplication/Controllers/BandArtistController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PerformanceApplication.Models;
+
+namespace PerformanceApplication.Controllers
+{
+    public class BandArtistController : Controller
+    {
+
+
+        // get list of all bands and artists
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // create an band or artist 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Get the post, to save it in the database.
+        [HttpPost]
+        public ActionResult CreateSave(string name, string description)
+        {
+            BandArtist band_artist = new BandArtist();
+            band_artist.insert(name, description);
+            //to do
+            // send values to model to save in database
+
+            return Content(name += description);
+        }
+
+    }
+}
---
+++ PerformanceApplication/Models/BandArtist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerformanceApplication.Models
+{
+    public class BandArtist
+    {
+        public string Name { get; set; }
+        public string Descripiton { get; set; }
+
+        public string insert(string name, string descripiton)
+        {
+            var sql = "Insert into band_artist name, description Values ('name', 'description')";
+            return sql;
+        }
+    }
+}
