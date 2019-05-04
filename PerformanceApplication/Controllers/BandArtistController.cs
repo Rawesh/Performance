00d35766d899c098bbe17e00b358e025b1eef6f4
Rawesh Chedi <99039154@mydavinci.nl>
2019-05-04 14:14:39 +0200

    Bands or artists are able to edit now

--- PerformanceApplication/Controllers/BandArtistController.cs
+++ PerformanceApplication/Controllers/BandArtistController.cs
@@ -41,5 +41,25 @@ namespace PerformanceApplication.Controllers
             return Redirect("index");
         }
 
+        public ActionResult Edit(int id)
+        {
+            //call model method getAll() that returns a dataset
+            //put the data in a dataset to use it in the view
+            DataSet ds = data.getOne(id);
+
+            // return the view with the dataset
+            return View(ds);
+        }
+
+        //Get the post, to save it in the database
+        [HttpPost]
+        public ActionResult EditSave(int id, string name, string description)
+        {
+            //call the insert method in the performance model with parameters
+            data.saveOne(id, name, description);
+
+            return Redirect("index");
+        }
+
     }
 }
--- PerformanceApplication/Models/BandArtist.cs
+++ PerformanceApplication/Models/BandArtist.cs
@@ -17,7 +17,7 @@ namespace PerformanceApplication.Models
         public string Descripiton { get; set; }
 
         //insert data in database
-        public void insert(string bandName, string bandDescription)
+        public void insert(string name, string description)
         {
             //open database connection
             SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
@@ -26,13 +26,12 @@ namespace PerformanceApplication.Models
             //insert the data in the database
             string insertQuery = "INSERT INTO band_artist(name, description) VALUES (@name, @description)";
             SqlCommand cmd = new SqlCommand(insertQuery, conn);
-            cmd.Parameters.AddWithValue("@name", bandName);
-            cmd.Parameters.AddWithValue("@description", bandDescription);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@description", description);
             cmd.ExecuteNonQuery();
 
             //close connection
             conn.Close();
-
         }
 
         public virtual DataSet getAll()
@@ -57,6 +56,48 @@ namespace PerformanceApplication.Models
                 }
             }
             return ds;
+        }
+
+        public virtual DataSet getOne(int id)
+        {
+            //make a set of data
+            DataSet ds = new DataSet();
+
+            //make connection with the database
+            string constr = ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                //Make connection wth database and send query to database
+                string query = "SELECT * FROM band_artist WHERE id =" + id;
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    //transfering the data from the dataset to the databsae with the SqlDataAdapter
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        //Adapter gets the data from database and fill the dataset with the data
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public void saveOne(int id, string name, string description)
+        {
+            //open database connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
+            conn.Open();
+
+            //Update the data in the database
+            string insertQuery = "UPDATE Band_artist SET name = @name, description = @description WHERE id = @id";
+            SqlCommand cmd = new SqlCommand(insertQuery, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@description", description);
+            cmd.ExecuteNonQuery();
+
+            //close connection
+            conn.Close();
         }
     }
 }
