5838337948edc789a0ab43dbab2349167c69909b
Rawesh Chedi <99039154@mydavinci.nl>
2019-05-05 16:31:21 +0200

    getall and edit works for stage

---
+++ PerformanceApplication/Controllers/StageController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using PerformanceApplication.Models; // use the model to call methods from the model
+
+namespace PerformanceApplication.Controllers
+{
+    public class StageController : Controller
+    {
+        // use data to acces te model
+        private Stage data = new Stage();
+
+        // get list of all bands and artists
+        public ActionResult Index()
+        {
+            //call model method getAll() that returns a dataset
+            //put the data in a dataset to use it in the view
+            DataSet ds = data.GetAll();
+
+            // return the view with the dataset
+            return View(ds);
+        }
+
+        // create an band or artist 
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //Get the post, to save it in the database
+        [HttpPost]
+        public ActionResult CreateSave(string name, string description)
+        {
+            //call the insert method in the performance model with parameters
+            data.Insert(name, description);
+
+            //redirect to index
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //call model method getOne() that returns a dataset
+            //put the data in a dataset to use it in the view
+            DataSet ds = data.GetOne(id);
+
+            // return the view with the dataset
+            return View(ds);
+        }
+
+        //Get the post, to save it in the database
+        [HttpPost]
+        public ActionResult EditSave(int id, string name, string description)
+        {
+            //call the update method in the performance model with parameters
+            data.SaveOne(id, name, description);
+
+            // redirect to index
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PerformanceApplication/Models/Stage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+namespace PerformanceApplication.Models
+{
+    public class Stage
+    {
+        public virtual DataSet GetAll()
+        {
+            //make a set of data
+            DataSet ds = new DataSet();
+
+            //make connection with the database
+            string constr = ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                //Make connection wth database and send query to database
+                string query = "SELECT * FROM stage";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    //transfering the data from the dataset to the databsae with the SqlDataAdapter
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        //Adapter gets the data from database and fill the dataset with the data
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return ds;
+        }
+
+        //insert data in database
+        public void Insert(string name, string description)
+        {
+            //open database connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
+            conn.Open();
+
+            //insert the data in the database
+            string insertQuery = "INSERT INTO stage(name, description) VALUES (@name, @description)";
+            SqlCommand cmd = new SqlCommand(insertQuery, conn);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@description", description);
+            cmd.ExecuteNonQuery();
+
+            //close connection
+            conn.Close();
+        }
+
+        public virtual DataSet GetOne(int id)
+        {
+            //make a set of data
+            DataSet ds = new DataSet();
+
+            //make connection with the database
+            string constr = ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                //Make connection wth database and send query to database
+                string query = "SELECT * FROM stage WHERE id =" + id;
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    //transfering the data from the dataset to the databsae with the SqlDataAdapter
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        //Adapter gets the data from database and fill the dataset with the data
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return ds;
+        }
+
+        public void SaveOne(int id, string name, string description)
+        {
+            //open database connection
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString);
+            conn.Open();
+
+            //Update the data in the database
+            string insertQuery = "UPDATE stage SET name = @name, description = @description WHERE id = @id";
+            SqlCommand cmd = new SqlCommand(insertQuery, conn);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@description", description);
+            cmd.ExecuteNonQuery();
+
+            //close connection
+            conn.Close();
+        }
+    }
+}
