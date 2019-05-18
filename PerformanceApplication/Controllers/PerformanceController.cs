daff110bd393b5420c06b4f8e768212505fd60ca
Rawesh Chedi <99039154@mydavinci.nl>
2019-05-18 11:54:38 +0200

    Database table, controller and model for performance

---
+++ PerformanceApplication/Controllers/PerformanceController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using PerformanceApplication.Models; // use the model to call methods from the model
+
+namespace PerformanceApplication.Controllers
+{
+    public class PerformanceController : Controller
+    {
+        // use data to acces te model (bandArtist, stage, performance)
+        private Performance performance = new Performance();
+        private BandArtist bandArtist = new BandArtist();
+        private Stage stage = new Stage();
+
+        // get a list of the performances
+        public ActionResult Index()
+        {
+            //call model method getAll() that returns a dataset
+            //put the data in a dataset to use it in the view
+            DataSet ds = performance.GetAll();
+
+            // return the view with the dataset
+            return View(ds);
+        }
+    }
+}
---
+++ PerformanceApplication/Models/Performance.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace PerformanceApplication.Models
+{
+    public class Performance
+    {
+        public virtual DataSet GetAll()
+        {
+            //make a set of data
+            DataSet ds = new DataSet();
+
+            //make connection with the database
+            string constr = ConfigurationManager.ConnectionStrings["DBconnection"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(constr))
+            {
+                //Make connection wth database and send query to database
+                string query = "SELECT band_artist.name, band_artist.description, performance.start_date, performance.end_date, stage.name" +
+                                " FROM performance " +
+                                "INNER JOIN band_artist ON performance.band_artist_id = band_artist.id " +
+                                "INNER JOIN stage ON performance.stage_id = stage.id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    //transfering the data from the dataset to the databsae with the SqlDataAdapter
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        //Adapter gets the data from database and fill the dataset with the data
+                        sda.Fill(ds);
+                    }
+                }
+            }
+            return ds;
+        }
+    }
+}
